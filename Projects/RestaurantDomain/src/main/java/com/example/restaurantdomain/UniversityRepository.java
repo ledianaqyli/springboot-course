0175ff9ea050a14e737a1078588125360d81907f
Lediana <ledianaqyli@gmail.com>
2023-04-24 02:17:06 +0200

    save

--- Projects/RestaurantDomain/src/main/java/com/example/restaurantdomain/UniversityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restaurantdomain;
-
-public interface UniversityRepository {
-
-    void information(String name,String faculty);
-    void feeInformation(String faculty, double fee);
-}
--- Projects/RestaurantDomain/src/main/java/com/example/restaurantdomain/UniversityService.java
+++
@@ -1,6 +0,0 @@
-package com.example.restaurantdomain;
-
-public interface UniversityService {
-        void chooseFaculties(String name,String faculty);
-        void payFee(String faculty, double fee);
-}
